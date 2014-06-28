c91a2eba7655edfd47ac598b45ca8723d4b26e59
Gabriel Barbier <barbier.gabriel@gmail.com>
2014-06-28 15:32:33 +0200

    Add generation flag to NOT overwrite manual changes.

--- generators/spring/plugins/org.obeonetwork.dsl.soa.gen.java.spring/src/org/obeonetwork/dsl/soa/gen/java/spring/main/ServiceMain.java
+++ generators/spring/plugins/org.obeonetwork.dsl.soa.gen.java.spring/src/org/obeonetwork/dsl/soa/gen/java/spring/main/ServiceMain.java
@@ -333,7 +333,7 @@ public class ServiceMain extends AbstractAcceleoGenerator {
 	 * 
 	 * @param resourceSet
 	 *            The resource set which registry has to be updated.
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public void registerPackages(ResourceSet resourceSet) {
