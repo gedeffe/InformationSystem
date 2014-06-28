6c5ee7ca40c9d9f92bf3c9801d8aaf6d49345f5d
Gabriel Barbier <barbier.gabriel@gmail.com>
2014-06-28 15:15:28 +0200

    Add generated elements for requirement editor.

---
+++ models/requirement/plugins/org.obeonetwork.dsl.requirement.editor/src/org/obeonetwork/dsl/requirement/parts/RequirementEditorViewsRepository.java
@@ -0,0 +1,24 @@
+/**
+ * Generated with Acceleo
+ */
+package org.obeonetwork.dsl.requirement.parts;
+
+/**
+ * 
+ * 
+ */
+public class RequirementEditorViewsRepository {
+
+	public static final int SWT_KIND = 0;
+
+	public static final int FORM_KIND = 1;
+
+
+	/**
+	 * main view descriptor
+	 * 
+	 */
+	public static class Main {
+	}
+
+}
---
+++ models/requirement/plugins/org.obeonetwork.dsl.requirement.editor/src/org/obeonetwork/dsl/requirement/requirement/providers/RequirementEEFAdapterFactory.java
@@ -0,0 +1,15 @@
+/**
+ * Generated with Acceleo
+ */
+package org.obeonetwork.dsl.requirement.requirement.providers;
+
+import org.obeonetwork.dsl.requirement.util.RequirementAdapterFactory;
+
+/**
+ * 
+ * 
+ */
+public class RequirementEEFAdapterFactory extends RequirementAdapterFactory {
+
+
+}
