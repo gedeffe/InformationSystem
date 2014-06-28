24f05edb9d708e8d39e33e4c2f6899f5a0bde305
Gabriel Barbier <barbier.gabriel@gmail.com>
2014-06-28 15:33:11 +0200

    Add generation flag to NOT overwrite manual changes.

--- generators/spring-hibernate/plugins/org.obeonetwork.dsl.soa.gen.java.spring.hibernate/src/org/obeonetwork/dsl/soa/gen/java/spring/hibernate/main/SpringHibernate.java
+++ generators/spring-hibernate/plugins/org.obeonetwork.dsl.soa.gen.java.spring.hibernate/src/org/obeonetwork/dsl/soa/gen/java/spring/hibernate/main/SpringHibernate.java
@@ -345,7 +345,7 @@ public class SpringHibernate extends AbstractAcceleoGenerator {
 	 * 
 	 * @param resourceSet
 	 *            The resource set which registry has to be updated.
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public void registerPackages(ResourceSet resourceSet) {
