e2e4638d6eeaaabe346ae7ee30c458e646a60cc8
Gabriel Barbier <barbier.gabriel@gmail.com>
2014-06-28 15:34:01 +0200

    Add generation flag to NOT overwrite manual changes.

--- generators/web/plugins/org.obeonetwork.informationsystem.gen.web/src/org/obeonetwork/informationsystem/gen/web/main/Main.java
+++ generators/web/plugins/org.obeonetwork.informationsystem.gen.web/src/org/obeonetwork/informationsystem/gen/web/main/Main.java
@@ -342,7 +342,7 @@ public class Main extends AbstractAcceleoGenerator {
 	 * 
 	 * @param resourceSet
 	 *            The resource set which registry has to be updated.
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public void registerPackages(ResourceSet resourceSet) {
